5c3a0bc7f5ba94c041f6cc9e9b24587b45a778bb
DarwinJS <DarwinJS@sanoys.com>
2017-04-09 17:00:44 -0400

    updated

--- pshcoredevenv/pshcoredevenv-debian.sh
+++ pshcoredevenv/pshcoredevenv-debian.sh
@@ -40,10 +40,10 @@ echo "Installing VS Code PowerShell Extension"
 code --install-extension ms-vscode.PowerShell
 
 argstring="'$*'"
-    if [[ $argstring =~ .*NONITERACTIVE.* ]] ; then
-        echo "Install Complete"
-    else
-        echo "Loading test code in VS Code"
-        wget -O ./testpowershell.ps1 https://raw.githubusercontent.com/DarwinJS/CloudyWindowsAutomationCode/master/pshcoredevenv/testpowershell.ps1
-        code ./testpowershell.ps1        
-    fi
+if [[ $argstring =~ NONITERACTIVE ]] ; then
+    echo "Install Complete"
+else
+    echo "Loading test code in VS Code"
+    wget -O ./testpowershell.ps1 https://raw.githubusercontent.com/DarwinJS/CloudyWindowsAutomationCode/master/pshcoredevenv/testpowershell.ps1
+    code ./testpowershell.ps1        
+fi
