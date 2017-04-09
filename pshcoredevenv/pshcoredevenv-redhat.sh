926b8d887ed44120e85b3233f2567ec10f929ffa
DarwinJS <DarwinJS@sanoys.com>
2017-04-09 10:33:10 -0400

    updated

--- pshcoredevenv/pshcoredevenv-redhat.sh
+++ pshcoredevenv/pshcoredevenv-redhat.sh
@@ -26,7 +26,7 @@ sudo yum install -y code
 echo "Installing VS Code PowerShell Extension"
 code --install-extension ms-vscode.PowerShell
 
-if [[ "$@" != "NONITERACTIVE" ]]; then
+if ! [[ "$@" == "NONITERACTIVE" ]]; then
   echo "Loading test code in VS Code"
   wget -O ./testpowershell.ps1 https://raw.githubusercontent.com/DarwinJS/CloudyWindowsAutomationCode/master/pshcoredevenv/testpowershell.ps1
   code ./testpowershell.ps1
