cd29e29aff0326fab4e05532437b6769217cda46
DarwinJS <DarwinJS@sanoys.com>
2017-04-09 10:20:34 -0400

    updated

--- pshcoredevenv/testpowershell.ps1
+++ pshcoredevenv/testpowershell.ps1
@@ -1,4 +1,4 @@
 
-#To test if VS Code is setup properly with PowerShell Core, press F5 and see if "PowerShell Dev Environment is Live!" shows up in an integrated PowerShell Window in VS Code.
+#To test if VS Code is setup properly with PowerShell Core, press F5 and see if "Congrats Your PowerShell Dev Environment is Live!" shows up in an integrated PowerShell Window in VS Code.
 
-Write-Output 'PowerShell Dev Environment is Live!'
+Write-Output 'Congrats Your PowerShell Dev Environment is Live!'
