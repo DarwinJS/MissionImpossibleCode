da8359d6111dc73abcebd4eb3979d31963db1270
DarwinJS <DarwinJS@sanoys.com>
2017-04-09 09:15:36 -0400

    added test powershell support

---
+++ pshcoredevenv/testpowershell.ps1
@@ -0,0 +1,4 @@
+
+#To test if VS Code is setup properly with PowerShell Core, press F5 and see if "PowerShell Dev Environment is Live!" shows up in an integrated PowerShell Window in VS Code.
+
+Write-Output 'PowerShell Dev Environment is Live!'
