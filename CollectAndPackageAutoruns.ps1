1fd920affbee2d28947ef3ad48a598d253798c93
DarwinJS <DarwinJS@sanoys.com>
2017-11-08 20:30:22 -0500

    fixes

--- CollectAndPackageAutoruns.ps1
+++ CollectAndPackageAutoruns.ps1
@@ -11,6 +11,12 @@ If (![bool]([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIde
 Invoke-WebRequest -Uri 'http://live.sysinternals.com/autoruns.exe'  -outfile "$env:public\autoruns.exe"
 cd $env:PUBLIC
 $Filename = "$env:PUBLIC\Autoruns-$env:Computername-$(Get-date -format 'yyMMdd-hhmmss').arn"
-Start-Process "$env:public\autoruns.exe" -ArgumentList "-e -a $Filename" -wait
+Write-Host "Starting autoruns - it is not unusual for it to run up to 3 minutes as it collects a lot of data."
+$ProcessHandle = Start-Process "$env:public\autoruns.exe" -ArgumentList "-e -a $Filename" -Passthru
+Do {
+    ++$ElapsedTime
+    Start-Sleep -Seconds 60
+    Write-Host "Been waiting $ElapsedTime Minutes..."
+} until ($ProcessHandle.HasExited -eq $True)
 Write-Host "Please send this file to the requester: `"$Filename`""
 Write-Host "You can also run `"$env:PUBLIC\autoruns.exe`" to open the file and examine what was collected."
