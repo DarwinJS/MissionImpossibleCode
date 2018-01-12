e6594a8c1b95aef32f06c8acc534aaa7bd12bc62
Darwin <darwinjsgithub.services@sanoys.com>
2018-01-11 20:45:32 -0500

    install-pwsh.ps1

--- install-pwsh.ps1
+++ install-pwsh.ps1
@@ -1,17 +1,30 @@
 <#
 Run this with: 
-Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/CloudyWindowsAutomationCode/master/install-pwsh.ps1')
+Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/CloudyWindowsAutomationCode/master/install-pwsh.ps1') ; Install-SysinternalsTool
+# Include IDE
+Invoke-Expression (invoke-webrequest -uri 'https://raw.githubusercontent.com/DarwinJS/CloudyWindowsAutomationCode/master/install-pwsh.ps1') ; Install-SysinternalsTool -includeide
+
 #>
 
-If (!(Test-Path env:chocolateyinstall)) 
-{
-  write-host "Installing Chocolatey..."
-  iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-}
-choco upgrade -y powershell-core visualstudiocode
-code --install-extension ms-vscode.PowerShell
+Function install-pwsh {
 
-Write-Host "*********************************************************************"
-Write-Host " How to setup core as the default PowerShell for visual studio code: "
-Write-Host "  https://github.com/PowerShell/PowerShell/blob/master/docs/learning-powershell/using-vscode.md"
-Write-Host
+  Param (
+    [Switch]$includeide
+    )
+
+  If (!(Test-Path env:chocolateyinstall)) 
+  {
+    write-host "Installing Chocolatey..."
+    iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+  }
+  If ($includeide)
+  {
+    choco upgrade -y powershell-core visualstudiocode
+    code --install-extension ms-vscode.PowerShell
+
+    Write-Host "*********************************************************************"
+    Write-Host " How to setup core as the default PowerShell for visual studio code: "
+    Write-Host "  https://github.com/PowerShell/PowerShell/blob/master/docs/learning-powershell/using-vscode.md"
+    Write-Host
+    }
+}
